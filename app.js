2a2f5f4f64d4a7df15824f7dc2a5a5e1cec1aff6
danieldanter <daniel.danter@gmx.at>
2021-10-15 20:54:01 +0200

    With Street name

--- app.js
+++ app.js
@@ -54,36 +54,35 @@ function displayEmployees(employeeData) {
 
 function displayModal(index) {
     // use object destructuring make our template literal cleaner
-    let { name, dob, phone, email, location: { city, street, state, postcode}, picture } = employees[index];
+    let { name, dob, phone, email, location: { city, street, state, postcode
+    }, picture } = employees[index];
     let date = new Date(dob.date);
-
     const modalHTML = `
     <img class="avatar" src="${picture.large}" />
     <div class="text-container">
-        <h2 class="name">${name.first} ${name.last}</h2>
-        <p class="email">${email}</p>
-        <p class="address">${city}</p>
-        <hr />
-        <p>${phone}</p>
-        <p class="address">${street}, ${state} ${postcode}</p>
-        <p>Birthday:
-        ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    <h2 class="name">${name.first} ${name.last}</h2>
+    <p class="email">${email}</p>
+    <p class="address">${city}</p>
+    <hr />
+    <p>${phone}</p>
+    <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
+    <p>Birthday:
+    ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
     </div>
     `;
-
     overlay.classList.remove("hidden");
     modalContainer.innerHTML = modalHTML;
-}
+    }
 
 
 gridContainer.addEventListener('click', e => {
-    // make sure the click is not on the gridContainer itself
-    if (e.target !== gridContainer) {
-        // select the card element based on its proximity to actual element clicked   
-        const card = e.target.closest(".card");
-        const index = card.getAttribute('data-index');
-        displayModal(index);
-    }
+        // make sure the click is not on the gridContainer itself
+        if (e.target !== gridContainer) {
+            
+            const card = e.target.closest(".card");
+            const index = card.getAttribute('data-index');
+            displayModal(index);
+        }
 });
 
 
